e0b25d11bd11499f57ebf35c3a558a469d15cc4e
akbala0404 <81943684+akbala0404@users.noreply.github.com>
2023-06-13 08:21:55 +0600

    remove

--- app/auth/router/router_create_new_shanyraq.py
+++ app/auth/router/router_create_new_shanyraq.py
@@ -1,37 +1,37 @@
-from fastapi import Depends, HTTPException, status
+# from fastapi import Depends, HTTPException, status
 
-from app.utils import AppModel
+# from app.utils import AppModel
 
-from ..service import Service, get_service
-from . import router
-
-
-class CreateListingRequest(AppModel):
-    type: str
-    price: int
-    address: str
-    area: float
-    rooms_count: int
-    description: str
+# from ..service import Service, get_service
+# from . import router
 
 
-class CreateListingResponse(AppModel):
-    _id: str
+# class CreateListingRequest(AppModel):
+#     type: str
+#     price: int
+#     address: str
+#     area: float
+#     rooms_count: int
+#     description: str
 
 
-@router.post(
-    "/shanyraks/",
-    status_code=status.HTTP_201_CREATED,
-    response_model=CreateListingResponse,
-)
-def create_listing(
-    request: CreateListingRequest,
-    svc: Service = Depends(get_service),
-) -> CreateListingResponse:
-    user_id = svc.get_current_user().user_id
-    listing_data = request.dict()
-    listing_data["user_id"] = user_id
+# class CreateListingResponse(AppModel):
+#     _id: str
 
-    listing_id = svc.create_listing(listing_data)
 
-    return CreateListingResponse(_id=str(listing_id))
+# @router.post(
+#     "/shanyraks/",
+#     status_code=status.HTTP_201_CREATED,
+#     response_model=CreateListingResponse,
+# )
+# def create_listing(
+#     request: CreateListingRequest,
+#     svc: Service = Depends(get_service),
+# ) -> CreateListingResponse:
+#     user_id = svc.get_current_user().user_id
+#     listing_data = request.dict()
+#     listing_data["user_id"] = user_id
+
+#     listing_id = svc.create_listing(listing_data)
+
+#     return CreateListingResponse(_id=str(listing_id))
