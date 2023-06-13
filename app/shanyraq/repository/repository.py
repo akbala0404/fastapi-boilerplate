8b4c27efe042e583143cb3194ff8e70d3b952a12
akbala0404 <81943684+akbala0404@users.noreply.github.com>
2023-06-13 21:52:33 +0600

    changes

--- app/shanyraq/repository/repository.py
+++ app/shanyraq/repository/repository.py
@@ -1,7 +1,8 @@
-# from datetime import datetime
+
 from typing import Any
 from bson.objectid import ObjectId
 from pymongo.database import Database
+from pymongo.results import DeleteResult, UpdateResult
 
 # from ..utils.security import hash_password
 
@@ -12,5 +13,22 @@ class ShanyraqRepository:
     
     def create_shanyraq(self, user_id: str, data: dict[str, Any]):
         data["user_id"] = ObjectId(user_id)
-        shanyraq = self.database["users"].insert_one(data)
+        shanyraq = self.database["shanyraq"].insert_one(data)
         return shanyraq.inserted_id
+    
+    def get_shanyraq(self, shanyraq_id: str):
+        return self.database["shanyraq"].find_one({"_id": ObjectId(shanyraq_id)})
+
+    def update_shanyraq(
+            self, shanyraq_id: str, user_id: str, data: dict[str, Any]) -> UpdateResult:
+        return self.database["shanyraq"].update_one(
+            filter={"_id": ObjectId(shanyraq_id), "user_id": ObjectId(user_id)},
+            update={
+                "$set": data,
+            },
+        )
+
+    def delete_shanyraq(self, shanyraq_id: str, user_id: str) -> DeleteResult:
+        return self.database["shanyraq"].delete_one(
+            {"_id": ObjectId(shanyraq_id), "user_id": ObjectId(user_id)}
+        )
