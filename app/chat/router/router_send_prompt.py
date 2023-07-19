5fd0cb2965c79e07122474ddf5f53684176cce6d
akbala0404 <81943684+akbala0404@users.noreply.github.com>
2023-07-19 09:32:40 +0600

    changes in fairytale creating

--- app/chat/router/router_send_prompt.py
+++ app/chat/router/router_send_prompt.py
@@ -13,6 +13,19 @@ class ChatRequest(AppModel):
 class ChatResponse(AppModel):
     response: str
 
+class FairytaleRequest(AppModel):
+    childAge: str
+    childGenger: str
+    storyLength: str
+    mainCharacter: str
+    description: str
+    moralOfStory: str
+
+class FairytaleResponse(AppModel):
+    title: str
+    readingTime: str
+    content: str
+
 @router.post("/", response_model=ChatResponse)
 def chat_with_ai(
     request: ChatRequest,
@@ -100,3 +113,48 @@ def chat_to_createFairytale(
         response = svc.chat_service.get_fairytale(prompt, systemPromt)
         content_text = response["content"] 
     return ChatResponse(response=content_text)
+
+@router.post("/createFairtale2", response_model=FairytaleResponse)
+def chat_to_createFairytale2(
+    request: FairytaleRequest,
+    svc: Service = Depends(get_service),
+) -> FairytaleResponse:
+    prompt = f"Пожалуйста, создайте сказку для ребенка {request.childAge} лет, {request.childGenger}. " \
+             f"Сказка должна быть продолжительностью {request.storyLength}. " \
+             f"Главным героем должен быть {request.description} по имени {request.mainCharacter}. " \
+             f"Мораль истории должна заключаться в том, чтобы {request.moralOfStory}."
+    # prompt = f"Давайте создадим удивительную сказку для ребенка {request.childAge} лет, {request.childGenger}. " \
+    #          f"Эта увлекательная история должна длиться примерно {request.storyLength}. " \
+    #          f"Главным героем станет {request.description} по имени {request.mainCharacter}, маленький исследователь и храбрец. " \
+    #          f"Путешествуя вместе с ним, ребенок отправится в захватывающее путешествие через волшебные миры и неизведанные земли." \
+    #          f"Но какая же сказка без волшебства и загадок? В каждом уголке мира герою предстоит преодолеть испытания и разгадать тайны." \
+    #          f"Возможно, на его пути появятся новые друзья и интересные союзники, которые помогут ему в сложных ситуациях." \
+    #          f"Светлый и темный магии переплетутся в этой удивительной истории, но главное – не забыть, что настоящая сила находится внутри каждого." \
+    #          f"И какой бы путь ни проложил герой, главная мораль этой сказки заключается в том, чтобы {request.moralOfStory}." \
+    #          f"Пусть ваш ребенок с удовольствием погрузится в этот мир волшебства и приключений и черпает вдохновение из каждой страницы этой уникальной истории!"
+    systemPromt = ("")
+    response = svc.chat_service.get_fairytale(prompt, systemPromt)
+    content_text = response["content"] 
+    fairy_tale = FairytaleResponse(**json.loads(content_text))
+    return fairy_tale
+    # return ChatResponse(response=content_text)
+
+@router.post("/explainTermsToChild", response_model=ChatResponse)
+def chat_to_explainTermsToChild(
+    request: ChatRequest,
+    svc: Service = Depends(get_service),
+) -> ChatResponse:
+    prompt = request.prompt
+    if "/explainChild" in prompt.lower():
+        newPromt = ("Ты родитель и тебе нудно обьяснить одну тему для ребенка")
+        systemPromt = ("Учитывай возраст и пол ребенка при объяснении. Это очень важно."
+                       "При объяснении учитывайте следующие аспекты, предоставленные родителем:"
+                       "- Возраст и стадия развития ребенка")
+        response = svc.chat_service.get_explainToChild(newPromt, systemPromt)
+        content_text = response["content"]
+    else:
+        systemPromt = ("")
+        response = svc.chat_service.get_explainToChild(prompt, systemPromt)
+        content_text = response["content"] 
+    return ChatResponse(response=content_text)
+
